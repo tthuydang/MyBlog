f07e3c3e5e5c6a2a0e59a9c3c31d8ce9cb8ff598
tt.huy.dang <tt.huy.dang@tomosia.com>
2020-07-03 10:14:34 +0700

    edited show and create actions and templates to display the blog input

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,22 @@ class ArticlesController < ApplicationController
 
   def new; end
 
+  def show
+    @article = Article.find(params[:id]) # id duoc tao tu dong va tang dan cho moi doi tuong Article dc tao ra
+  end
+
   # bắt sự kiện submit
   def create
-    render plain: params[:article].inspect
+    # render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to @article # this will redirect it to the show page and display the article we just created
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
