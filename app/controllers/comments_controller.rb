44e71ce62a7e06bdc7eec71f7bd09476a7b54615
tt.huy.dang <tt.huy.dang@tomosia.com>
2020-07-07 13:20:07 +0700

    comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,9 +2,9 @@ class CommentsController < ApplicationController
 
   def create
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
+    @comment = @article.comments.create(comment_params) # comments la 1 function chu ko phai thuoc tinh, va cung ko phai la 1 mang cho nen ko goi ham append/add dc
 
-    redirect_to article_path(@article)
+    redirect_to article_path(@article) # truyen @article qua cho trang show su dung
   end
 
   private
