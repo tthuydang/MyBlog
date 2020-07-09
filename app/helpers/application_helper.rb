95cd2bf0573e237c1837c5cb518a7dc10e405aa6
tt.huy.dang <tt.huy.dang@tomosia.com>
2020-07-09 10:07:16 +0700

    Added a custom helper method for active links in navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # if on active page => color = white
+  # if not on active page => color = regular
+  # takes arguments: 'link', which controller, 'what page its on', 'url', 'change font color'
+  def active_link_to(nav_link, controller, action, url, style)
+    if controller_name == controller && action_name == action # index, new, show, edit,...
+      link_to(nav_link, url, id: style)
+    else
+      link_to(nav_link, url, class: style)
+    end
+  end
 end
